0e83f141162357afd53ce021f52eeb109a5b3086
sidddharthsingh <sidsingh1679@gmail.com>
2023-10-21 17:14:17 +0530

    hashing

---
+++ hashing.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int count(int n, vector<int> arr)
+{
+    int con = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == n)
+        {
+            con++;
+        }
+    }
+    return con;
+}
+
+int main()
+{
+    vector<int> arr = {0, 2, 1, 3, 4, 2, 4, 2, 4};
+    int res = count(2, arr);
+    cout << res << endl;
+    return 0;
+}
