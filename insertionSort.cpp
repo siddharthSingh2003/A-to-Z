b1dbdddaa418ea483292ede77bafe4c9930e017f
sidddharthsingh <sidsingh1679@gmail.com>
2023-10-27 12:52:08 +0530

    insertion

---
+++ insertionSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+
+    insertionSort(arr, n);
+
+    std::cout << "\nSorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
