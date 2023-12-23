500ff5875460f4d78a47dc6c5d964181aebaa114
sidddharthsingh <sidsingh1679@gmail.com>
2023-12-24 01:54:06 +0530

    contains duplicate

---
+++ containDuplicate.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+bool containsDuplicate(const std::vector<int> &nums)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[j] == nums[i])
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    std::vector<int> user_input;
+    int num;
+
+    std::cout << "Enter integers (enter a non-integer to finish):" << std::endl;
+    while (std::cin >> num)
+    {
+        user_input.push_back(num);
+    }
+
+    bool hasDuplicate = containsDuplicate(user_input);
+
+    if (hasDuplicate)
+    {
+        std::cout << "The array contains duplicate elements." << std::endl;
+    }
+    else
+    {
+        std::cout << "No duplicates found in the array." << std::endl;
+    }
+
+    return 0;
+}
