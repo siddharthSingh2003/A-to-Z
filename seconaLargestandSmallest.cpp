4e2611460d9f9a097744d74b65963f6e3e2bb0c8
sidddharthsingh <sidsingh1679@gmail.com>
2023-12-26 01:02:39 +0530

    better

--- seconaLargestandSmallest.cpp
+++ seconaLargestandSmallest.cpp
@@ -1,33 +1,81 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+// Brute Approch
+// int getElement(int arr[], int n)
+// {
+//     int SecondSmall = 0, SecondLargest = 0;
+//     sort(arr, arr + n);
+//     int small = arr[0];
+//     int large = arr[n - 1];
+//     for (int i = 1; i < n; i++)
+//     {
+//         if (small != arr[i])
+//         {
+//             SecondSmall = arr[i];
+//             break;
+//         }
+//     }
+//     for (int i = n - 2; i >= 0; i--)
+//     {
+//         if (large != arr[i])
+//         {
+//             SecondLargest = arr[i];
+//             break;
+//         }
+//     }
+
+//     cout << "Second Smallest =>" << SecondSmall << endl;
+//     cout << "Second Largest =>" << SecondLargest << endl;
+
+//     return 0;
+// }
+
+// Better Approch
 
 int getElement(int arr[], int n)
 {
-    int SecondSmall = 0, SecondLargest = 0;
-    sort(arr, arr + n);
+    // code here
+
+    int largest = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
     int small = arr[0];
-    int large = arr[n - 1];
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if (small != arr[i])
+        if (arr[i] < small)
         {
-            SecondSmall = arr[i];
-            break;
-        }
-    }
-    for (int i = n - 2; i >= 0; i--)
-    {
-        if (large != arr[i])
-        {
-            SecondLargest = arr[i];
-            break;
+            small = arr[i];
         }
     }
 
-    cout << "Second Smallest =>" << SecondSmall << endl;
-    cout << "Second Largest =>" << SecondLargest << endl;
+    int secondLargest = -1;
 
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > secondLargest && arr[i] != largest)
+        {
+            secondLargest = arr[i];
+        }
+    }
+
+    int secondSmall = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < secondSmall && arr[i] != small)
+        {
+            secondSmall = arr[i];
+        }
+    }
+    cout << secondLargest << endl;
+    cout << secondSmall << endl;
     return 0;
 }
 
