705880480d30a58248d48eea9e251d3e527a17a2
sidddharthsingh <sidsingh1679@gmail.com>
2024-01-03 16:22:40 +0530

    solution 2

--- longestSubarrayWithSumK.cpp
+++ longestSubarrayWithSumK.cpp
@@ -1,19 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 // brute force
+// int longestSubarray(int arr[], int n, int K)
+// {
+//     int length;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i; j < n; j++)
+//         {
+//             int sum = 0;
+
+//             for (int k = i; k <= j; k++)
+//             {
+//                 sum += arr[k];
+//             }
+
+//             if (sum == K)
+//             {
+//                 length = max(length, j - i + 1);
+//             }
+//         }
+//     }
+
+//     cout << length << endl;
+
+//     return 0;
+// }
+
 int longestSubarray(int arr[], int n, int K)
 {
-    int length;
+    int length = 0;
     for (int i = 0; i < n; i++)
     {
+        int sum = 0;
         for (int j = i; j < n; j++)
         {
-            int sum = 0;
 
-            for (int k = i; k <= j; k++)
-            {
-                sum += arr[k];
-            }
+            sum += arr[j];
 
             if (sum == K)
             {
@@ -26,7 +49,6 @@ int longestSubarray(int arr[], int n, int K)
 
     return 0;
 }
-
 int main()
 {
     int arr[] = {2, 3, 5, 1, 9};
