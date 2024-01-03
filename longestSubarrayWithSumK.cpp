c99b96c4be782aa8fac4bedeb9900fcb103233a3
sidddharthsingh <sidsingh1679@gmail.com>
2024-01-03 16:04:00 +0530

    longest suubarray 1

---
+++ longestSubarrayWithSumK.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+// brute force
+int longestSubarray(int arr[], int n, int K)
+{
+    int length;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int sum = 0;
+
+            for (int k = i; k <= j; k++)
+            {
+                sum += arr[k];
+            }
+
+            if (sum == K)
+            {
+                length = max(length, j - i + 1);
+            }
+        }
+    }
+
+    cout << length << endl;
+
+    return 0;
+}
+
+int main()
+{
+    int arr[] = {2, 3, 5, 1, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int K = 10;
+    longestSubarray(arr, n, K);
+    return 0;
+}
