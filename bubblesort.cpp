98f0c05440478f40cd4ad4ec9b350343afa02a4f
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2024-01-13 23:55:56 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -19,7 +19,7 @@ void bubbleSort(int arr[], int n)
 
 int main()
 {
-    int arr[] = {04, 234, 25, 12, 222, 10, 39};
+    int arr[] = {04, 234, 25, 12, 22, 10, 39};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     std::cout << "Original array: ";
