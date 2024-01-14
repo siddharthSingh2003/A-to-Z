3b7b550975fe63ded749760d2dc8369a49559735
sidddharthsingh <sidsingh1679@gmail.com>
2024-01-15 00:25:07 +0530

    pascal

---
+++ pascalTriangle2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pascalTriangle(int n)
+{
+    long long ans = 1;
+    cout << ans << " "; // printing 1st element
+
+    // Printing the rest of the part:
+    for (int i = 1; i < n; i++)
+    {
+        ans = ans * (n - i);
+        ans = ans / i;
+        cout << ans << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n = 144;
+    pascalTriangle(n);
+    return 0;
+}
