918a0e1c7e0b0b402ab64446ab86d2ab031e2786
sidddharthsingh <sidsingh1679@gmail.com>
2026-01-29 12:20:59 +0530

    new starting

---
+++ binarySearch.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int low = 0, n = nums.size(), high = n - 1;
+
+        while (low < high)
+        {
+
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (target > nums[mid])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
--- linked List/convertarr2LL.cpp
+++ linked List/convertarr2LL.cpp
@@ -5,21 +5,22 @@ class Node
 {
 public:
     int data;
+
     Node *next;
 
-public:
-    Node(int data1)
+    Node(int data)
     {
-        data = data1;
-        next = nullptr;
+        this->data = data;
+        this->next = NULL;
     }
 };
 
-Node* convertarr2LL(vector<int> &arr){
+Node *convertArray2LL(vector<int> &arr)
+{
     Node *head = new Node(arr[0]);
     Node *mover = head;
-
-    for (int i = 0; i < arr.size(); i++){
+    for (int i = 1; i < arr.size(); i++)
+    {
         Node *temp = new Node(arr[i]);
         mover->next = temp;
         mover = mover->next;
@@ -27,8 +28,19 @@ Node* convertarr2LL(vector<int> &arr){
     return head;
 }
 
-int main(){
+int printLL(Node* head ){
+
+    Node *temp = head;
+    while(temp!=NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    return 0;
+}
+
+int main()
+{
     vector<int> arr = {1, 2, 3, 4, 5};
-    Node *head = convertarr2LL(arr);
-    cout << head->data << endl;
+    Node *head = convertArray2LL(arr);
+    printLL(head);
 }
